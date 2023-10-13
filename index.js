2291c1bb42ec13fb08df45738625e85194c266e8
Adolf <adolfnyerere@gmail.com>
2023-10-14 02:31:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+
+  The program performs a linear time operation on each number in a list of integers by iterating through the list using a for loop. 
+  It performs a constant-time operation by comparing the current number (num) through the length of the array.
 */
 
 /* 
   Add your pseudocode here
+
+  Iterate through each number in the array.
+For the current number, determine a complementary number that, 
+when added to it, sums up to the target (complement = target - number).
+
+Continue iterating through the rest of the array.
+
+  If any number in the array matches the complement, return "true."
+  If no match is found in the entire array, return "false."
 */
 
 /*
